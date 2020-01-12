9af60328f1249ceabcbfc20e83dea3dd9140a013
Kira <kawoods826@gmail.com>
2020-01-11 16:51:38 -0800

    created reducer for counter

---
+++ src/reducers/counter-reducer.js
@@ -0,0 +1,14 @@
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+reducer(initialState, { type: "increment" });
+reducer(initialState, { type: "decrement" });
